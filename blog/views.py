7542f1efa201d96c687730eee577fb99883810cd
Ivan_Andrianov <andreyanovi@yandex.ru>
2023-10-03 19:19:43 +0300

    Add_paginator

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,26 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
     """Метод post_list принимает request, извлекает все посты со статусом PUBLISHED, используя менеджер published"""
     posts = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу результатов
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
